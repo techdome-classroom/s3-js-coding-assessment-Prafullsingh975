4c8ee52bf17f60aceb781f5972bf746280f3361a
Prafull Singh <prafullsingh975@gmail.com>
2024-10-07 15:22:00 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const brackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    const result = [];
+
+    for (let char of s) {
+        if (char in brackets) {
+            const firstElement = result.length ? result.pop() : '#';
+
+            if (brackets[char] !== firstElement) {
+                return false;
+            }
+        } else {
+            result.push(char);
+        }
+    }
+
+    return result.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,7 +3,32 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    var romanToInt = function(s) {
+        const romanLetter = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
     
+        let res = 0;
+    
+        for (let i = 0; i < s.length; i++) {
+            let curr = romanLetter[s[i]];
+            let nex = romanLetter[s[i + 1]];
+    
+            if (nex && curr < nex) {
+                res -= curr;
+            } else {
+                res += curr;
+            }
+        }
+    
+        return res;
+    };
 };
 
 
